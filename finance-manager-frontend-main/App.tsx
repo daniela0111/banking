118cc6d4bcd3fa64a5ecc8967c4c5e31e43c832b
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-04-28 23:53:56 +0200

    camera update

--- finance-manager-frontend-main/App.tsx
+++ finance-manager-frontend-main/App.tsx
@@ -1,5 +1,5 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 import MainNavigation from './screens/MainNavigation';
@@ -10,27 +10,70 @@ import { useEffect, useRef, useState } from 'react';
 import {Camera} from 'expo-camera';
 import {shareAsync} from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library'
+import { StatusBar } from 'expo-status-bar';
 
 // Create a client
 const queryClient = new QueryClient()
+
+return (
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <MainNavigation />  
+  </QueryClientProvider>
+</Provider>
+);
 
 export default function App() {
   let cameraRef = useRef();
   const [hasCameraPermission, setHasCameraPermission] = useEffect();
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const  [photo, setPhoto] = useState();
 
   useEffect(() => {
     (async () => {
-      const  cameraStatus = await Camera.requestPermissionsAsync();
-      const mediaLibStatus = await MediaLibrary.getAuthorizationStatusAsync();
-  return (
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <MainNavigation />  
-      </QueryClientProvider>
-    </Provider>
-  );
-}
+      const  cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+      })();
+    }, []);
+
+    if (!hasCameraPermission === undefined) {
+      return <Text>Requesting for camera permission</Text>; // Camera permission is not determined yet
+      } else if (!hasCameraPermission) {
+        return <Text>No access to camera. Please set your camera</Text>; // Camera permission is denied
+    }
+
+    let takePhoto = async () => {
+      let options = {
+        quality: 1,
+        base64: true,
+        exif:  false,
+        };
+    
+        let newPhoto = await cameraRef.current.takePhotoAsync(options);
+        setPhoto(newPhoto);
+      };
+
+      if (photo) {
+        let sharePhoto = () => {
+
+        };
+        let savePhoto = () => {
+
+        };
+      }
+    }
+
+    return (
+      <Camera style={styles.container} ref={cameraRef}>
+        <View style={styles.buttonContainer}> 
+          <Button title="Take Photo" onPress={takePhoto} />
+        </View>
+        <StatusBar style='auto'/>
+      </Camera>
+    );
+    };
 
 const styles = StyleSheet.create({
   container: {
@@ -39,4 +82,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    backgroundColor: '#fff',
+    alignSelf: 'flex-end',
 });
